81d1d00e592f81d393e6cbd83d2e854368cbd4b1
IlincaMurarescu <ilincamurarescu@gmail.com>
2024-03-22 23:42:02 +0200

    fixed bug

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./styles/global.css";
 import Home from "./components/Home";
+import React from "react";
 
 function App() {
   return (
--- vite-project/src/components/Home/index.jsx
+++ vite-project/src/components/Home/index.jsx
@@ -3,19 +3,28 @@ import styles from "./Home.module.css";
 import styled from "styled-components";
 import { tweets } from "../../utils/tweets";
 import Tweet from "../Tweet";
+import CreatePostForm from "../CreatePostForm";
 
 const Feed = styled.div`
-  max-width: 42 rem;
-  width: 60%;
+  max-width: 42rem;
+  width: 100%;
+  border-left: 1px solid #475569;
+  border-right: 1px solid #475569;
+  max-width: 672px;
 `;
 
 export default function Home() {
   return (
-    <main className={styles.feed}>
-      <Feed className="w-2 border-x border-slate-400">
-        {tweets.map((tweet) => (
-          <Tweet></Tweet>
-        ))}
+    <main className={styles.home}>
+      <Feed>
+        <div className="border-b border-slate-400 p-4">
+          <CreatePostForm />
+        </div>
+        <div className="flex flex-col">
+          {tweets.map((tweet) => (
+            <Tweet key={tweet.id} tweet={tweet} />
+          ))}
+        </div>
       </Feed>
     </main>
   );
